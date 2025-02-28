c05eee43ab2a66690b868a788538f31c2ba22718
Krushan Bauva <krushan@bauva.com>
2025-02-28 18:10:08 +0530

    add down migration

--- local-to-cloud/migrations/data/1740743952993_add_new_table_product_reviews/down.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ONLY public.product_reviews
-    DROP CONSTRAINT product_reviews_product_id_fkey;
-
-ALTER TABLE ONLY public.product_reviews
-    DROP CONSTRAINT product_reviews_user_id_fkey;
-
-DROP TABLE IF EXISTS public.product_reviews;
-
-DROP SEQUENCE IF EXISTS public.product_reviews_id_seq;
